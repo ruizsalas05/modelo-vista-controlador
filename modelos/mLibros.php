9949ed80788344732722ef14e19450b6167da5e2
ruizsalas05 <luchitosalas015@gmail.com>
2025-05-16 09:49:12 -0500

    Create mLibros.php

---
+++ modelos/mLibros.php
@@ -0,0 +1,11 @@
+<?php
+    class mLibros{
+        private $datosBD;
+        private $libros;
+
+        function __construct(){
+            $this->datosBD = conexion::conexion();
+            $this->libros = 
+        }
+    }
+?>
